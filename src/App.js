36c85b8fb55e36c3aa24823ea02b06acb113e97c
Truc Nguyen <trucn0215@gmail.com>
2021-02-25 22:31:39 -0800

    turn the App.js to ES6 with the initial states

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from "react";
 import Header from "./components/Header";
 import Body from "./components/Body"
 
-function App() {
-  return (
-    <div className="container">
-      <Header/>
-      <Body/>
-    </div>
-  );
+class App extends React.Component{
+
+  state = {
+    users: []
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <Header/>
+        <Body/>
+      </div>
+    );
+  }
 }
 
 export default App;
