64a73627bc6e62a859708e4bb436047e0063e5d6
Truc Nguyen <trucn0215@gmail.com>
2021-02-26 21:01:24 -0800

    adding sub-titles for user info

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,12 +3,12 @@ import React from "react";
 function Body({ users }) {
     return (
         <div className="">
-            {/* <div className="card-body row">
+            <div className="card-body row">
                 <h5 className="card-title col">Name</h5>
                 <h5 className="card-title col">Date of Birth</h5>
                 <h5 className="card-title col">Email</h5>
                 <h5 className="card-title col">Phone</h5>
-            </div> */}
+            </div>
 
             <div className="card-body border border-primary row">
                 <div className="col">{users.map(users => <p className="userName card-text">{users.name.first} {users.name.last}</p>)}</div>
