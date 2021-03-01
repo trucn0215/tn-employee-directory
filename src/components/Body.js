732b23ad764334251c583791ce34826db6499d94
Truc Nguyen <trucn0215@gmail.com>
2021-02-28 21:37:32 -0800

    styling the subtitles and align the info

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,22 +3,25 @@ import React from "react";
 function Body({ users }) {
     return (
         <div className="usersTable">
-            <div className="card-body row">
-                <h5 className="col">Image</h5>
-                <h5 className="col">Name</h5>
-                <h5 className="col">Date of Birh5</h5>
-                <h5 className="col">Email</h5>
-                <h5 className="col">Phone</h5>
+            <div className="row mb-3 bg-info text-white font-weight-bold pt-2">
+                <h5 className="col-2"> Image</h5>
+                <h5 className="col-2">Name</h5>
+                <h5 className="col-1">DoB</h5>
+                <h5 className="col-4">Email</h5>
+                <h5 className="col-3">Phone</h5>
             </div>
-
-            <div className="card-body border border-primary row">
-                <div className="col">
+            <div className="border border-primary row pt-2 pb-2">
+                <div className="col-2">
                     {users.map(users => <img src={users.picture.medium} alt="h5umbnail"/>)}
                 </div>
-                <div className="col">{users.map(users => <p className="userName card-text">{users.name.first} {users.name.last}</p>)}</div>
-                <div className="col">{users.map(users => <p className="age card-text">{users.dob.age}</p>)}</div>
-                <div className="col">{users.map(users => <p className="email card-text">{users.email}</p>)}</div>
-                <div className="col">{users.map(users => <p className="cell card-text">{users.cell}</p>)}</div>
+                <h5 className="col-2"> {users.map(users => <p className="userName card-text">{users.name.first} {users.name.last}</p>)}
+                </h5>
+
+                <h5 className="col-1">
+                {users.map(users => <p className="age card-text">{users.dob.age}</p>)}
+                </h5>
+                <h5 className="col-4">{users.map(users => <p className="email">{users.email}</p>)}</h5>
+                <h5 className="col-3">{users.map(users => <p className="cell">{users.cell}</p>)}</h5>
             </div>
         </div>
     );
